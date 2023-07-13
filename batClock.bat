d96e357f197dbc018b6faf35c26c1be6ed337497
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-07-13 00:01:01 -0300

    finish clock

--- batClock.bat
+++ batClock.bat
@@ -2,12 +2,16 @@
 Title batClock
 @mode con cols=30 lines=7
 color 03
+set seconds=0
 :main
 cls
+SET /A "seconds=seconds+1"
 echo.
 echo Time: %time%
 echo.
 echo Date: %date%
+echo.
+echo Passaram: %seconds% segundos
 echo.
 
 ping -n 2 0.0.0.0 >nul
